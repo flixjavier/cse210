d39091eecb5140838c7f5e5eeffb4bbdbed0b719
flixjavier <flixjavier@gmail.com>
2022-06-20 16:05:58 -0600

    added snake game

---
+++ snake-incomplete/snake/game/scripting/move_actors_action.py
@@ -0,0 +1,25 @@
+from game.scripting.action import Action
+
+
+# TODO: Implement MoveActorsAction class here! 
+
+class MoveActorsAction(Action):
+    #** This class moves all the actors in the cast.
+
+    def __init__(self):
+        super().__init__()
+
+    def execute(self, cast, script):
+        """ Args:
+            cast (Cast): The cast of Actors in the game.
+            script (Script): The script of Actions in the game. """
+        # Override the execute(cast, script) method as follows:
+        # 1) get all the actors from the cast
+        actors = cast.get_all_actors()
+        # 2) loop through the actors
+        for actor in actors:
+            actor.move_next()
+        # 3) call the move_next() method on each actor
+       
+     
+
