78a034827378cbf7805512f6989969b2c8ad6a2b
flixjavier <flixjavier@gmail.com>
2022-06-21 18:53:15 -0600

    added some comments to moveactorsaction

--- snake-incomplete/snake/game/scripting/move_actors_action.py
+++ snake-incomplete/snake/game/scripting/move_actors_action.py
@@ -10,6 +10,14 @@ class MoveActorsAction(Action):
         super().__init__()
 
     def execute(self, cast, script):
+        """
+        "Loop through all the actors in the cast and call the move_next() method on each one."
+        
+        Now let's look at the move_next() method
+        
+        :param cast: The cast of Actors in the game
+        :param script: The script of Actions in the game
+        """
         """ Args:
             cast (Cast): The cast of Actors in the game.
             script (Script): The script of Actions in the game. """
